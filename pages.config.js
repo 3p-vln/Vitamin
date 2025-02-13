524c23c0a4e33209c2a947b1957b4fc9741ef7a4
3p-vln <vldknovak@gmail.com>
2025-02-13 18:08:50 +0200

    add page for deploy

--- pages.config.js
+++ pages.config.js
@@ -5,6 +5,10 @@ const pages = [
     name: 'index',
     path: resolve(__dirname, 'index.html'),
   },
+  {
+    name: 'quiz',
+    path: resolve(__dirname, 'quiz.html'),
+  },
 ];
 
 export default pages;
