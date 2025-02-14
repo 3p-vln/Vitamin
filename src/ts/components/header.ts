40dcc7c71c91c0fc8d2475a9cd0c414c53c71206
3p-vln <vldknovak@gmail.com>
2025-02-14 23:12:51 +0200

    header.ts in functions

--- src/ts/components/header.ts
+++ src/ts/components/header.ts
@@ -1,88 +1,79 @@
 import { getElement } from '../composables/useCallDom';
 
-class Header {
-  burgerBtn: HTMLElement | null;
-  menuMain: HTMLElement | null;
-  bagBtn: HTMLElement | null;
-  logoBtn: HTMLElement | null;
+const burgerBtn = getElement('.burger__btn');
+const menuMain = getElement('.burger__menu.main');
+const bagBtn = getElement('.header__bag');
+const logoBtn = getElement('.header__logo');
 
-  constructor() {
-    this.burgerBtn = getElement('.burger__btn');
-    this.bagBtn = getElement('.header__bag');
-    this.logoBtn = getElement('.header__logo');
-    this.menuMain = getElement('.burger__menu.main');
+export function initHeader() {
+  if (!menuMain) return;
 
-    this.init();
-  }
+  const shopBtn = getElement('.menu__item_shop', menuMain);
+  const infoBtn = getElement('.menu__item_info', menuMain);
+  const profileBtn = getElement('.header__profile', menuMain);
+  const shopMenu = getElement('.burger__menu.shop');
+  const infoMenu = getElement('.burger__menu.info');
+  const profileMenu = getElement('.burger__menu.profile');
 
-  init() {
-    if (!this.menuMain) {
-      return;
-    }
-
-    const shopBtn = getElement('.menu__item_shop', this.menuMain);
-    const infoBtn = getElement('.menu__item_info', this.menuMain);
-    const profileBtn = getElement('.header__profile', this.menuMain);
-    const shopMenu = getElement('.burger__menu.shop');
-    const infoMenu = getElement('.burger__menu.info');
-    const profileMenu = getElement('.burger__menu.profile');
-
-    if (this.menuMain && this.burgerBtn && shopBtn && shopMenu && infoBtn && infoMenu && profileBtn && profileMenu) {
-      this.burgerToggle('burger__btn', 'main');
-      this.burgerToggle('menu__item_shop', 'shop');
-      this.burgerToggle('menu__item_info', 'info');
-      this.burgerToggle('header__profile_adapt', 'profile');
-      this.burgerBack('shop__title', 'shop');
-      this.burgerBack('info__title', 'info');
-      this.burgerBack('profile__title', 'profile');
-    }
-  }
-
-  burgerToggle(clickBtn: string, elActive: string) {
-    if (clickBtn) {
-      const btn = getElement(`.${clickBtn}`);
-      const el = getElement(`.${elActive}`);
-      if (btn && el) {
-        btn.addEventListener('click', () => {
-          if (el.classList.contains('main')) {
-            el.classList.toggle(`${elActive}_active`);
-            this.hideBag();
-            btn.classList.toggle(`${clickBtn}_active`);
-          } else {
-            el.classList.add(`${elActive}_active`);
-          }
-        });
-      } else {
-        console.log('Кнопка или нужное меню не найдено');
-      }
-    }
-  }
-
-  burgerBack(clickBtn: string, elActive: string) {
-    if (clickBtn) {
-      const btn = getElement(`.${clickBtn} .back-btn`);
-      const el = getElement(`.${elActive}`);
-      if (btn && el) {
-        btn.addEventListener('click', () => {
-          el.classList.remove(`${elActive}_active`);
-        });
-      } else {
-        console.log('Кнопка или нужное меню не найдено');
-      }
-    }
-  }
-
-  hideBag() {
-    if (this.bagBtn && this.logoBtn && this.menuMain) {
-      if (this.menuMain.classList.contains('main_active')) {
-        this.logoBtn.style.opacity = '0';
-        this.bagBtn.style.display = 'none';
-      } else {
-        this.logoBtn.style.opacity = '1';
-        this.bagBtn.style.display = 'block';
-      }
-    }
+  if (burgerBtn && shopBtn && shopMenu && infoBtn && infoMenu && profileBtn && profileMenu) {
+    burgerToggle('burger__btn', 'main');
+    burgerToggle('menu__item_shop', 'shop');
+    burgerToggle('menu__item_info', 'info');
+    burgerToggle('header__profile_adapt', 'profile');
+    burgerBack('shop__title', 'shop');
+    burgerBack('info__title', 'info');
+    burgerBack('profile__title', 'profile');
   }
 }
 
-export const header = new Header();
+function burgerToggle(clickBtn: string, elActive: string) {
+  if (clickBtn) {
+    const btn = getElement(`.${clickBtn}`);
+    const el = getElement(`.${elActive}`);
+
+    if (btn && el) {
+      btn.addEventListener('click', () => {
+        if (el.classList.contains('main')) {
+          el.classList.toggle(`${elActive}_active`);
+          hideBag();
+          btn.classList.toggle(`${clickBtn}_active`);
+
+          return;
+        }
+        el.classList.add(`${elActive}_active`);
+      });
+
+      return;
+    }
+    console.log('Кнопка или нужное меню не найдено');
+  }
+}
+
+function burgerBack(clickBtn: string, elActive: string) {
+  if (clickBtn) {
+    const btn = getElement(`.${clickBtn} .back-btn`);
+    const el = getElement(`.${elActive}`);
+    if (btn && el) {
+      btn.addEventListener('click', () => {
+        el.classList.remove(`${elActive}_active`);
+      });
+
+      return;
+    }
+    console.log('Кнопка или нужное меню не найдено');
+  }
+}
+
+function hideBag() {
+  if (bagBtn && logoBtn && menuMain) {
+    if (menuMain.classList.contains('main_active')) {
+      logoBtn.style.opacity = '0';
+      bagBtn.style.display = 'none';
+
+      return;
+    }
+
+    logoBtn.style.opacity = '1';
+    bagBtn.style.display = 'block';
+  }
+}
