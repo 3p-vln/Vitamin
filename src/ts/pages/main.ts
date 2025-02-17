353efd34ae73fb17c3e5da3d740667bfd58e608a
sergey-uhanov <uhanov.sergio@gmail.com>
2025-02-17 19:35:02 +0200

    home: Find out what vitamins

--- src/ts/pages/main.ts
+++ src/ts/pages/main.ts
@@ -1,16 +1,16 @@
 import { initHeader } from '../components/header';
-import { register } from '../composables/useApi';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
-  console.log(
-    register({
-      first_name: 'Test',
-      last_name: 'Test',
-      email: 'novkvlaa@gmail.com',
-      password: 'qwerty123Shjgkg',
-      role_type: 'regular',
-    })
-  );
+  // console.log(
+  //   register({
+  //     first_name: 'Test',
+  //     last_name: 'Test',
+  //     email: 'novkvlaa@gmail.com',
+  //     password: 'qwerty123Shjgkg',
+  //     role_type: 'regular',
+  //   })
+  // );
 
   initHeader();
 });
