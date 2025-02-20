0136cd2cdb3d692606f98cdac07f05e24a8a7732
3p-vln <vldknovak@gmail.com>
2025-02-20 02:15:28 +0200

    shop: desktop & adapt & logik

--- src/ts/components/render-card.ts
+++ src/ts/components/render-card.ts
@@ -1,4 +1,4 @@
-import { getRecommendations } from '../composables/useApi';
+import { getCatalogList, getRecommendations } from '../composables/useApi';
 import { classManipulator, getElement, renderElement } from '../composables/useCallDom';
 
 interface RecommendationData {
@@ -15,13 +15,16 @@ interface RecommendationResponse {
   errors?: { message: string }[];
 }
 
-export async function renderProdCard(container: string) {
+let currentPage = 1;
+const itemsPerPage = 10;
+const itemsViewMore = 6;
+const windowWidth = window.innerWidth;
+
+export async function renderRecCard(container: string) {
   const prodContainer = getElement(container);
   if (!prodContainer) return;
 
   const response = (await getRecommendations(false)) as RecommendationResponse;
-
-  console.log(response);
 
   if (response.errors) {
     return;
@@ -29,12 +32,49 @@ export async function renderProdCard(container: string) {
 
   const prodList = response.data;
 
-  await card(prodList, prodContainer);
+  await card(prodList, prodContainer, 'white');
 }
 
-async function card(data: RecommendationData[], container: HTMLElement) {
-  data.forEach((prodItem) => {
-    const card = renderElement('a', ['prod-card', `${prodItem.id}`]);
+export async function renderAllCard(container: string, page: number = 1) {
+  const prodContainer = getElement(container);
+  if (!prodContainer) return;
+
+  const response = (await getCatalogList(page, itemsPerPage)) as RecommendationResponse;
+
+  if (response.errors) {
+    return;
+  }
+
+  const prodList = response.data;
+
+  // totalProducts = response.data.length;
+
+  await card(prodList, prodContainer, 'gray');
+
+  updatePagination(page, container);
+
+  if (windowWidth < 768) {
+    handleViewMoreButtonVisibility();
+  }
+}
+
+async function card(data: RecommendationData[], container: HTMLElement, colour: string) {
+  let cardsToShow = 10;
+
+  if (windowWidth < 768) {
+    cardsToShow = 6;
+  }
+
+  if (windowWidth < 375) {
+    cardsToShow = 4;
+  }
+
+  // totalProducts = cardsToShow;
+
+  const productsToRender = data.slice(0, cardsToShow);
+
+  productsToRender.forEach((prodItem) => {
+    const card = renderElement('a', ['prod-card', `${prodItem.id}`, `prod-card_${colour}`]);
     const cardContainer = renderElement('div', 'prod-card__content');
 
     const cardImg = renderElement('div', 'prod-card__img');
@@ -110,7 +150,6 @@ async function card(data: RecommendationData[], container: HTMLElement) {
     card.appendChild(cardContainer);
 
     container.appendChild(card);
-    console.log(prodItem.id);
   });
 }
 
@@ -120,4 +159,69 @@ function getDiscountedPrice(price: string, discount: number): number {
     throw new Error('Invalid price format');
   }
   return +(originalPrice * (1 - discount / 100)).toFixed(2);
+}
+
+function updatePagination(page: number, container: string) {
+  const paginationContainer = getElement('.catalog-list__pagination');
+  if (!paginationContainer) return;
+
+  const pages = Math.ceil(100 / itemsPerPage);
+
+  const containerElement = getElement(container);
+  if (!containerElement) return;
+
+  paginationContainer.innerHTML = '';
+
+  const prevButton = renderElement('button', ['catalog-list__btn', 'catalog-list__btn_prew']);
+  prevButton.innerHTML = `<svg><use href="#back-arrow"></use></svg>`;
+  prevButton.onclick = () => handlePageChange(page - 1, containerElement);
+  paginationContainer.appendChild(prevButton);
+
+  for (let i = 1; i <= pages; i++) {
+    const pageButton = renderElement('button', ['catalog-list__btn', 'catalog-list__btn_page']);
+
+    if (page === i) {
+      classManipulator(pageButton, 'add', 'catalog-list__btn_active');
+    }
+
+    pageButton.innerText = `${i}`;
+    pageButton.onclick = () => handlePageChange(i, containerElement);
+    paginationContainer.appendChild(pageButton);
+  }
+
+  const nextButton = renderElement('button', ['catalog-list__btn', 'catalog-list__btn_next']);
+  nextButton.innerHTML = `<svg><use href="#back-arrow"></use></svg>`;
+  nextButton.onclick = () => handlePageChange(page + 1, containerElement);
+  paginationContainer.appendChild(nextButton);
+}
+
+function handlePageChange(newPage: number, container: HTMLElement) {
+  if (newPage < 1 || newPage > Math.ceil(100 / itemsPerPage)) return;
+  currentPage = newPage;
+
+  window.history.pushState({}, '', `?page=${newPage}`);
+
+  container.innerHTML = '';
+
+  renderAllCard(`.${container.className}`, currentPage);
+}
+
+function handleViewMoreButtonVisibility() {
+  const viewMoreButton = getElement('.catalog-list__view-more');
+  if (!viewMoreButton) return;
+
+  const totalPages = Math.ceil(20 / itemsViewMore);
+
+  if (currentPage < totalPages) {
+    viewMoreButton.style.display = 'flex';
+  } else {
+    viewMoreButton.style.display = 'none';
+  }
+
+  viewMoreButton.onclick = () => {
+    if (currentPage < totalPages) {
+      currentPage += 1;
+      renderAllCard('.catalog-list__content', currentPage);
+    }
+  };
 }
--- src/ts/pages/shop.ts
+++ src/ts/pages/shop.ts
@@ -1,7 +1,13 @@
 import { initHeader } from '../components/header';
+import { renderAllCard } from '../components/render-card';
+// import { getCatalogList } from '../composables/useApi';
 import { shopBaners } from '../shop/shop-baners';
 
 document.addEventListener('DOMContentLoaded', async () => {
   initHeader();
   shopBaners();
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = parseInt(urlParams.get('page') || '1', 10);
+  renderAllCard('.catalog-list__content', page);
+  // renderAllCard('.catalog-list__content');
 });
