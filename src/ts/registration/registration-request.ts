72d34f65b29003e0de517b5392dda1e91d1a3eb3
sergey-uhanov <uhanov.sergio@gmail.com>
2025-02-23 19:03:26 +0200

    home: fix feedback slider

---
+++ src/ts/registration/registration-request.ts
@@ -0,0 +1,13 @@
+import { RegisterData, register } from '../composables/useApi.ts';
+
+export async function  registrationRequest(data: RegisterData) {
+  try {
+    const res : any = await register(data);
+    console.log(res);
+    if(res.statusCode === 201) {
+      const res : any = await register(data);
+      console.log(res.data);
+    }
+  }catch (error) {}
+
+}
