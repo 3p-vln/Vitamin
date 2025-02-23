4eb5ffddde3d58693361eb2d9b801f1b277a838d
3p-vln <vldknovak@gmail.com>
2025-02-23 21:39:45 +0200

    fix import

--- src/ts/registration/registration-request.ts
+++ src/ts/registration/registration-request.ts
@@ -1,13 +1,13 @@
-import { RegisterData, register } from '../composables/useApi.ts';
+import { RegisterData } from './../components/interfaces';
+import { register } from '../composables/useApi.ts';
 
-export async function  registrationRequest(data: RegisterData) {
+export async function registrationRequest(data: RegisterData) {
   try {
-    const res : any = await register(data);
+    const res: any = await register(data);
     console.log(res);
-    if(res.statusCode === 201) {
-      const res : any = await register(data);
+    if (res.statusCode === 201) {
+      const res: any = await register(data);
       console.log(res.data);
     }
-  }catch (error) {}
-
+  } catch (error) {}
 }
