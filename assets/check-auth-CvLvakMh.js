a49e1ad85a40b2c25a1ebb4deeb5308037c9a842
3p-vln <vldknovak@gmail.com>
2025-03-03 20:59:06 +0200

    Updates

---
+++ assets/check-auth-CvLvakMh.js
@@ -0,0 +1,1 @@
+import{b as n}from"./axios-BrcDDtdF.js";const t=n.get("accessToken"),i=window.location.pathname;!t&&!i.includes("/login.html")&&!i.includes("/registration.html")&&(window.location.href="/Vitamin/login.html");t&&(i.includes("/login.html")||i.includes("/registration.html"))?window.location.href="/Vitamin":document.body.style.visibility="visible";
