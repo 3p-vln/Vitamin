c165d791e53452bf264296dd15eedd98f8573cf2
sergey-uhanov <uhanov.sergio@gmail.com>
2025-03-03 08:00:27 +0200

    recover password end

--- src/ts/reset-password/set-password-request.ts
+++ src/ts/reset-password/set-password-request.ts
@@ -1,18 +1,31 @@
 import apiClient from '../registration/api-client.ts';
 import { AxiosResponse } from 'axios';
 
-
-export async function setPasswordRequest(data: { resetToken: string, newPassword: string }) {
-
+export async function setPasswordRequest(data: { resetToken: string; newPassword: string }) {
+  const messageContainer: HTMLElement | null = document.querySelector('.recover__message');
+  const form: HTMLElement | null = document.querySelector('.recover__form_new-password');
   try {
     const res = await apiClient.post(`/auth/set-new-password`, data);
     console.log(res);
     if (res.status === 200) {
-
-      window.location.href = '/Vitamin';
+      if (messageContainer && form) {
+        form.remove()
+        messageContainer.innerHTML = `The password was successfully changed  <a class="recover__link-login" href="/Vitamin/login.html"> Back to login</a>`;
+        messageContainer.classList.remove('recover__message_hidden');
+        messageContainer.style.color = 'green'
+      }
     }
   } catch (error) {
     const errorAxios = error as AxiosResponse;
-    console.log(errorAxios);
+
+    switch (errorAxios.status) {
+      case 400:
+        if (messageContainer){
+          messageContainer.innerHTML = `Oops. Error, the password has not been changed. Try again later.<a class="recover__link-login" href="/Vitamin/login.html"> Back to login</a>`;
+          messageContainer.classList.remove('recover__message_hidden');
+          messageContainer.style.color = 'red'
+
+        }
+    }
   }
 }
