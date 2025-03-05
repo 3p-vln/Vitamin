489c1cf5aef8c26465fb5e1b46e368a7ba64d0d6
3p-vln <vldknovak@gmail.com>
2025-03-05 17:54:27 +0200

    add form for create order: desktop & adapt without logic

--- src/ts/pages/create-order.ts
+++ src/ts/pages/create-order.ts
@@ -1,7 +1,9 @@
 import { createOrderList } from '../create-order/order-list.ts';
 import { initAccordion } from '../create-order/order-accordion.ts';
+import { createOrderHeader } from '../create-order/header.ts';
 
 document.addEventListener('DOMContentLoaded', async () => {
   createOrderList();
   initAccordion();
+  createOrderHeader()
 });
