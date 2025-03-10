7e3c8bea3bfabc34aa4a721150d765ba8d1c1195
3p-vln <vldknovak@gmail.com>
2025-03-10 21:49:02 +0200

    skeletons for one product page

--- src/ts/pages/one-product.ts
+++ src/ts/pages/one-product.ts
@@ -2,10 +2,16 @@ import { initHeader } from '../components/header';
 import { renderRecCard } from '../components/render-card';
 import { addToCartBtn } from '../one-product/add-to-cart';
 import { loadInfo } from '../one-product/load-info';
+import { stop } from '../components/stopPreload.ts';
+import { removeSkeletons } from '../components/removeSkeletons.ts';
 
 document.addEventListener('DOMContentLoaded', async () => {
   initHeader();
   await loadInfo();
   await addToCartBtn();
   await renderRecCard('.you-like__cards', 'gray');
+
+  stop();
 });
+
+await removeSkeletons();
