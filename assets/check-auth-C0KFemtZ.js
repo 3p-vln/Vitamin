744c15b0b4733308e34e4cd88b383dda8ffd81d5
3p-vln <vldknovak@gmail.com>
2025-03-10 22:37:07 +0200

    Updates

---
+++ assets/check-auth-C0KFemtZ.js
@@ -0,0 +1,1 @@
+import{c as n}from"./use-api-Cn90IiIh.js";const t=n.get("accessToken"),i=window.location.pathname;!t&&!i.includes("/login.html")&&!i.includes("/registration.html")&&(window.location.href="/Vitamin/login.html");t&&(i.includes("/login.html")||i.includes("/registration.html"))?window.location.href="/Vitamin":document.body.style.visibility="visible";
---
+++ assets/stopPreload-C_2Hv9rs.js
@@ -0,0 +1,1 @@
+function o(){const n=new Event("loadingIsFinished");document.dispatchEvent(n),console.log(n)}export{o as s};
