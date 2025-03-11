de73da66025d898954f1847e67ead5dc2f2d44c8
3p-vln <vldknovak@gmail.com>
2025-03-11 12:35:47 +0200

    Updates

---
+++ assets/stopPreload-B1kA71Tc.js
@@ -0,0 +1,1 @@
+function s(){const n=new CustomEvent("loadingIsFinished");document.dispatchEvent(n)}export{s};
--- assets/stopPreload-C_2Hv9rs.js
+++
@@ -1,1 +0,0 @@
-function o(){const n=new Event("loadingIsFinished");document.dispatchEvent(n),console.log(n)}export{o as s};
