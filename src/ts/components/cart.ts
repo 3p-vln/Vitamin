4949f9b61d29ae37f4a93bff062c3598d10c5980
3p-vln <vldknovak@gmail.com>
2025-03-12 12:09:13 +0200

    cart: add list of prod to bag

--- src/ts/components/cart.ts
+++ src/ts/components/cart.ts
@@ -22,6 +22,7 @@ export async function initCart() {
   cartBtn.addEventListener('click', (event) => cartActive(event));
 
   cartCloseBtn.addEventListener('click', () => cartClose());
+  cartBg.addEventListener('click', () => cartClose());
 
   loadCartFromLocalStorage();
   totalCartPrice();
@@ -275,9 +276,12 @@ export function renderProdCard(prod: Product, autoshipChecked: boolean = false, 
   empty = false;
   emptyBag(empty);
 
-  saveProductToLocalStorage(prod);
+  const autoshipEl = getElement('.autoship__on-off');
+  const countsItemsEl = getElement(`.count__items`);
 
-  // prodList = getElements(`.prod`);
+  if (!autoshipEl || !countsItemsEl) saveAllProductToLocalStorage(prod);
+  else saveProductToLocalStorage(prod);
+
   removeProd(prod.id);
 
   updateInfoInLocal(prod);
@@ -324,6 +328,23 @@ function saveProductToLocalStorage(prod: Product) {
       autoshipChecked: autoshipActive,
       autoshipDays: '30',
       counts: countsItems,
+    });
+  }
+
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
+
+function saveAllProductToLocalStorage(prod: Product) {
+  let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+  const productExists = cartItems.some((item: Product) => item.id === prod.id);
+
+  if (!productExists) {
+    cartItems.push({
+      id: prod.id,
+      autoshipChecked: true,
+      autoshipDays: '30',
+      counts: 1,
     });
   }
 
@@ -611,7 +632,6 @@ function blockBtn() {
   let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
 
   if (cartItems.length === 0) {
-    console.log(cartItems.length);
     btn.style.backgroundColor = '#C3BDB6';
     btn.style.pointerEvents = 'none';
     return;
@@ -619,4 +639,37 @@ function blockBtn() {
 
   btn.style.backgroundColor = '';
   btn.style.pointerEvents = '';
+}
+
+export async function addAllToCart(prodsId: string[]) {
+  for (const prodId of prodsId) {
+    try {
+      let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+      const prod = await getCatalogItem(prodId);
+
+      if ('errors' in prod) {
+        console.error(prod);
+        return;
+      }
+
+      const productExists = cartItems.some((item: Product) => item.id === prod.id);
+
+      if (!productExists) {
+        renderProdCard(prod, false, '30', 1);
+        totalCartPrice();
+        blockBtn();
+        continue;
+      }
+
+      const counterItems = getElement(`.prod_${prod.id} .counter__items`);
+      if (!counterItems) return;
+
+      updateAutoshipInLocalStorage(`${prod.id}`, false, '30', Number(counterItems.textContent) + 1);
+      loadCartFromLocalStorage();
+
+      totalCartPrice();
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
--- src/ts/pages/personal-pack.ts
+++ src/ts/pages/personal-pack.ts
@@ -4,12 +4,14 @@ import { unfixBtn } from '../personal-pack/fix-btn';
 import { changeName } from '../personal-pack/pack-info';
 import { stop } from '../components/stopPreload.ts';
 import { removeSkeletons } from '../components/removeSkeletons.ts';
+import { addRecToCart } from '../personal-pack/add-rec-to-cart.ts';
 
 document.addEventListener('DOMContentLoaded', async () => {
   initHeader();
   changeName();
   await renderRecCard('.your-pack__cards', 'white');
   unfixBtn();
+  await addRecToCart();
   stop();
 });
 
