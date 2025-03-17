11d8f422d4b681af94925cfdf9a62426bbb695f0
3p-vln <vldknovak@gmail.com>
2025-03-17 20:43:44 +0200

    fix cart: logic of total summ, scroll cart in create order

--- src/ts/create-order/order-list.ts
+++ src/ts/create-order/order-list.ts
@@ -3,7 +3,7 @@ import { ProductLocalStorge } from '../../typings/interfaces.ts';
 import { getCatalogItem } from '../composables/use-api.ts';
 import { getTotalPrice, getDiscountedPrice } from '../components/cart/render-cart.ts';
 
-const orderListCintainer = getElement('.order-list__prods');
+const orderListCintainer = getElement('.order-list__prods-container');
 const orderListPrice = getElement('.order-list__total');
 
 export default function createOrderList() {
@@ -93,13 +93,13 @@ async function renderProd(prod: ProductLocalStorge) {
   }
 }
 
-function totalCartPrice() {
+async function totalCartPrice() {
   if (!orderListPrice) return;
 
   let cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
 
   let total: number = 0;
-  let totalProdPrice: string;
+  let totalProdPrice: number;
   let discount: number = 0;
 
   const subtotalPrice = renderElement('p', 'order-list__subtotal');
@@ -112,8 +112,8 @@ function totalCartPrice() {
     totalPrice.innerHTML = `Today’s Total: <span>$0</span>`;
   }
 
-  cartItems.forEach(async (item: ProductLocalStorge) => {
-    try {
+  try {
+    for (const item of cartItems) {
       const prodItem = await getCatalogItem(`${item.id}`);
 
       if ('errors' in prodItem) {
@@ -122,48 +122,63 @@ function totalCartPrice() {
       }
 
       if (prodItem.type === 'Sale%') {
-        totalProdPrice = getDiscountedPrice(prodItem.price, prodItem.discount, item.counts);
-        discount += parseFloat(getTotalPrice(prodItem.price, item.counts).replace(/,/g, '').replace(/\s/g, '')) - parseFloat(getDiscountedPrice(prodItem.price, prodItem.discount, item.counts).replace(/,/g, '').replace(/\s/g, ''));
-      } else totalProdPrice = getTotalPrice(prodItem.price, item.counts);
+        totalProdPrice = parseFloat(getTotalPrice(prodItem.price, item.counts).replace(/,/g, '').replace(/\s/g, ''));
 
-      total += parseFloat(totalProdPrice.replace(/,/g, '').replace(/\s/g, ''));
+        const discountedPrice = parseFloat(getDiscountedPrice(prodItem.price, prodItem.discount, item.counts).replace(/,/g, '').replace(/\s/g, ''));
 
-      subtotalPrice.innerHTML = `Subtotal <span>$${total.toLocaleString('en-US', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      })}</span>`;
+        discount += totalProdPrice - discountedPrice;
 
-      orderListPrice.appendChild(subtotalPrice);
+        console.log('prod price: ' + totalProdPrice);
 
-      if (discount !== 0) {
-        discountPrice.innerHTML = `Discount <span class="red">-$${discount.toLocaleString('en-US', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
-        })}</span>`;
+        total += Math.round(totalProdPrice * 100) / 100;
+      } else {
+        totalProdPrice = parseFloat(getTotalPrice(prodItem.price, item.counts).replace(/,/g, '').replace(/\s/g, ''));
 
-        orderListPrice.appendChild(discountPrice);
+        console.log('prod price: ' + totalProdPrice);
+
+        total += Math.round(totalProdPrice * 100) / 100;
       }
+    }
 
-      shoppingPrice.innerHTML = 'Shipping <span>$9.20</span>';
-      orderListPrice.appendChild(shoppingPrice);
+    console.log('total: ' + total.toFixed(2));
 
-      total += 9.2;
+    subtotalPrice.innerHTML = `Subtotal <span>$${total.toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}</span>`;
 
-      totalPrice.innerHTML = `Today’s Total: <span>$${total.toLocaleString('en-US', {
+    orderListPrice.appendChild(subtotalPrice);
+
+    if (discount !== 0) {
+      discountPrice.innerHTML = `Discount <span class="red">-$${discount.toLocaleString('en-US', {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2,
       })}</span>`;
 
-      orderListPrice.appendChild(totalPrice);
+      orderListPrice.appendChild(discountPrice);
 
-      if (!accordionTotalPrice) return;
-
-      accordionTotalPrice.innerText = `$${total.toLocaleString('en-US', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-      })}`;
-    } catch (error) {
-      console.log(error);
+      total -= discount;
     }
-  });
+
+    shoppingPrice.innerHTML = 'Shipping <span>$9.20</span>';
+    orderListPrice.appendChild(shoppingPrice);
+
+    total += 9.2;
+
+    totalPrice.innerHTML = `Today’s Total: <span>$${total.toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}</span>`;
+
+    orderListPrice.appendChild(totalPrice);
+
+    if (!accordionTotalPrice) return;
+
+    accordionTotalPrice.innerText = `$${total.toLocaleString('en-US', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}`;
+  } catch (error) {
+    console.error(error);
+  }
 }
