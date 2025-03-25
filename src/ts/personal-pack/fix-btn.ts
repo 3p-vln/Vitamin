78cd4b10012e9120cc729cb47f0bb8b196dec7d5
3p-vln <vldknovak@gmail.com>
2025-03-25 23:16:53 +0200

    fix personal pack btn

--- src/ts/personal-pack/fix-btn.ts
+++ src/ts/personal-pack/fix-btn.ts
@@ -1,4 +1,6 @@
-import { getElement } from '../composables/use-call-dom.ts';
+import { classManipulator, getElement } from '../composables/use-call-dom.ts';
+
+let timeout: ReturnType<typeof setTimeout>;
 
 export function unfixBtn() {
   const container = getElement('.your-pack');
@@ -6,15 +8,33 @@ export function unfixBtn() {
 
   if (!container || !button) return;
 
-  if (window.innerWidth <= 768) {
-    window.addEventListener('scroll', () => {
-      const containerRect = container.getBoundingClientRect();
+  window.addEventListener('scroll', () => {
+    fixBtn(container, button);
+  });
 
-      if (containerRect.top <= 0 && containerRect.bottom <= 920) {
-        button.classList.remove('your-pack__continue_fixed');
-        return;
+  window.addEventListener('resize', () => {
+    fixBtn(container, button);
+  });
+
+  fixBtn(container, button);
+}
+
+function fixBtn(container: HTMLElement, button: HTMLElement) {
+  if (window.innerWidth <= 768) {
+    const rect = container.getBoundingClientRect();
+    const distanceToBottom = window.innerHeight - rect.bottom;
+
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      if (distanceToBottom < -150) {
+        classManipulator(button, 'add', 'your-pack__continue_fixed');
+      } else {
+        classManipulator(button, 'remove', 'your-pack__continue_fixed');
       }
-      button.classList.add('your-pack__continue_fixed');
-    });
+    }, 100);
+
+    return;
   }
+
+  classManipulator(button, 'remove', 'your-pack__continue_fixed');
 }
