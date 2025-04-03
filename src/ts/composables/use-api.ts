ba6c6251bd2e8f5a89b80dc22ed4ea07255a797a
sergey-uhanov <uhanov.sergio@gmail.com>
2025-04-03 16:05:31 +0300

    fix bugs

--- src/ts/composables/use-api.ts
+++ src/ts/composables/use-api.ts
@@ -104,7 +104,7 @@ const clearAuthData = (): void => {
   Cookies.remove(ACCESS_TOKEN_KEY, { path: '/' });
   Cookies.remove(REFRESH_TOKEN_KEY, { path: '/' });
   localStorage.removeItem('userInfo');
-  window.location.href = '/Vitamin//login.html';
+  window.location.href = '/Vitamin/login.html';
 };
 
 // Перехватчик запросов: добавляет заголовок Authorization с токеном, если он есть
